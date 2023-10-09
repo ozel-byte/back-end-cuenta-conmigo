4338925c362b9a0e1d4a90f75f924bb4bbca683b
ozel-byte <ozelcode@gmail.com>
2023-10-08 20:42:06 -0600

    Upload project

---
+++ src/main/java/com/cicla/CuentaConmigo/CuentaConmigoApplication.java
@@ -0,0 +1,13 @@
+package com.cicla.CuentaConmigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentaConmigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuentaConmigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cicla/CuentaConmigo/CuentaConmigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicla.CuentaConmigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentaConmigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
